715795355b21413d9f16ff440057dcf4dc154ce7
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2025-01-26 17:56:28 +0000

    lines between points of wave

--- particle.gd
+++ particle.gd
@@ -1,21 +1,27 @@
 extends RigidBody2D
 
-@export var x_velocity = 0;
-@export var y_velocity = 0;
-@export var speed_min = 5;
+@export var x_velocity = 0
+@export var y_velocity = 0
+@export var min_speed = 40
+@export var slowdown_per_sec = 0.5
+@export var slowdown_low_support_per_sec = 0.1
 
 @onready var sprite = $Sprite2D
-@onready var particle_finder = $ParticleFinder
+@onready var particle_hitbox = $ParticleFinder/ParticleSupport
 
 var test = false
 var starting_velocity
 var color_vec = [1,1,1]
-var supporting_particles = []
+var supporting_particles_angles = {}
 
-# Called when the node enters the scene tree for the first time.
+var delay = true
+var num_supporting = 0
+
 func _ready() -> void:
 	linear_velocity = Vector2(x_velocity, y_velocity)
 	starting_velocity = linear_velocity.length()
+	particle_hitbox.disabled = true
+	#sprite.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -23,29 +29,53 @@ func _physics_process(delta: float) -> void:
 	if handle_visual_strength():
 		get_parent().remove_particle(self)
 		queue_free()
-	
-	get_support()
-	
-	#slowdown(0.99)
+	if not delay:
+		calc_angles()
+		#queue_redraw()
+		handle_slowdown(delta)
 
 func handle_move(delta: float) -> void:
 	var collision = move_and_collide(linear_velocity * delta)
 	if collision:
 		linear_velocity = linear_velocity.bounce(collision.get_normal())
+	sprite.rotation = linear_velocity.angle() + deg_to_rad(90)
+	particle_hitbox.get_parent().rotation = linear_velocity.angle() + deg_to_rad(90)
 
-func get_support():
-	var factor = 0.95
-	for particle in supporting_particles:
+func calc_angles():
+	for particle in supporting_particles_angles:
 		if is_instance_valid(particle):
-			var angle = rad_to_deg(linear_velocity.normalized().angle_to(particle.get_speed().normalized()))
-			if test: print(angle)
-			if angle < 15 and angle > -15: factor += 0.01
+			supporting_particles_angles[particle] = rad_to_deg(linear_velocity.normalized().angle_to(particle.get_speed().normalized()))
+
+func handle_slowdown(delta):
+	num_supporting = 0
+	for particle in supporting_particles_angles:
+		if is_instance_valid(particle):
+			if abs(supporting_particles_angles[particle]) < 10: 
+				num_supporting += 1
+				if test: 
+					particle.color_vec = [0,1,0]
+			elif test: particle.color_vec = [1,0,0]
+
+	var factor = 1 - (slowdown_per_sec * delta)
+	if num_supporting == 1:
+		factor = 1 - (slowdown_low_support_per_sec * delta)
+	elif num_supporting > 1:
+		factor = 1
 	slowdown(factor)
+
+func _draw():
+	if num_supporting > 1:
+		for particle in supporting_particles_angles:
+			if is_instance_valid(particle):
+				if abs(supporting_particles_angles[particle]) < 10: 
+					draw_line(Vector2.ZERO, particle.position-position, Color(0, 1, 1,
+					(linear_velocity.length()-min_speed)/(starting_velocity-min_speed)
+					), 5)
 
 func handle_visual_strength() -> bool:
 	sprite.modulate = Color(color_vec[0],color_vec[1],color_vec[2], 
-			(linear_velocity.length()-speed_min)/(starting_velocity-speed_min))
-	return linear_velocity.length() < speed_min
+			(linear_velocity.length()-min_speed)/(starting_velocity-min_speed))
+	return linear_velocity.length() < min_speed
 
 func set_tester():
 	color_vec = [1,1,0]
@@ -60,16 +90,22 @@ func get_location():
 func slowdown(factor):
 	self.linear_velocity = linear_velocity*factor
 
+func start_checking_for_slowdown():
+	delay = false
+	particle_hitbox.disabled = false
+
 func _on_particle_finder_body_entered(body: Node2D) -> void:
-	if get_parent().is_particle(body) and body != self:
-		if body not in supporting_particles:
-			supporting_particles.append(body)
-			if test: 
-				body.color_vec = [1,0,0]
+	if not delay:
+		if get_parent().is_particle(body) and body != self:
+			if body not in supporting_particles_angles:
+				supporting_particles_angles[body] = 0
+				if test: 
+					body.color_vec = [1,0,0]
 
 func _on_particle_finder_body_exited(body: Node2D) -> void:
-	if get_parent().is_particle(body):
-		if body in supporting_particles:
-			supporting_particles.erase(body)
-			if test: 
-				body.color_vec = [1,1,1]
+	if not delay:
+		if get_parent().is_particle(body):
+			if body in supporting_particles_angles:
+				supporting_particles_angles.erase(body)
+				if test: 
+					body.color_vec = [1,1,1]
--- wave.gd
+++ wave.gd
@@ -4,14 +4,23 @@ var particle = load("res://particle.tscn")
 
 @export var wave_speed = 50
 @export var num_particles = 4
-@export var loc_sim_norm = 100
-@export var speed_sim_norm = 100
+@export var slowdown_per_sec = 0.80
+@export var slowdown_low_support_per_sec = 0.2
+@export var dist_until_slowdown = 40
+@export var min_particle_speed = 40
 
+@export var focus_angle = 0.2
+@export var focus_intensity = 0.5
+
+var focus_dir = 0
+var slowed = false
 var particles = []
 var test = true
+var t = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	focus_dir = randf_range(0, 360)
 	var angle = 360.0/num_particles
 	var current_angle = 0
 	while current_angle < 360:
@@ -19,11 +28,24 @@ func _ready() -> void:
 		if test:
 			instance.set_tester()
 			test = false
-		instance.x_velocity = cos(deg_to_rad(current_angle)) * wave_speed
-		instance.y_velocity = sin(deg_to_rad(current_angle)) * wave_speed
+		instance.x_velocity = cos(deg_to_rad(current_angle+focus_dir)) * wave_speed
+		instance.y_velocity = sin(deg_to_rad(current_angle+focus_dir)) * wave_speed
+		instance.slowdown_per_sec = slowdown_per_sec
+		instance.min_speed = min_particle_speed
+		instance.slowdown_low_support_per_sec = slowdown_low_support_per_sec
 		add_child(instance)
 		particles.append(instance)
 		current_angle += angle
+
+func _process(delta):
+	if not slowed: 
+		t = t + delta
+		if t * wave_speed > 20:
+			for part in particles:
+				if part.delay:
+					part.start_checking_for_slowdown()
+	if particles == []:
+		queue_free()
 
 func remove_particle(part):
 	particles.erase(part)
