eaa4f55bd10130dc9eef18c5fddcdea5823898f6
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2025-02-02 14:59:48 +0000

    make wave change based on mouse position

--- mouse_click.gd
+++ mouse_click.gd
@@ -2,10 +2,16 @@ extends Node
 
 var wave = load("res://wave.tscn")
 
+var waiting_for_wave = false
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	if Input.is_action_just_released("mouseClick"):
+	if Input.is_action_just_pressed('mouseClick') and not waiting_for_wave:
+		waiting_for_wave = true
 		var instance = wave.instantiate()
 		instance.position = get_viewport().get_mouse_position()
 		
 		add_child(instance)
+
+func wave_spawned():
+	waiting_for_wave = false
--- wave.gd
+++ wave.gd
@@ -3,66 +3,69 @@ extends Node2D
 var particle = load("res://particle.tscn")
 
 @export var wave_speed = 50
-@export var num_particles = 4
+@export var total_particles = 4
+
 @export var slowdown_per_sec = 0.80
 @export var slowdown_low_support_per_sec = 0.2
 @export var dist_until_slowdown = 40
 @export var min_particle_speed = 40
 
-@export var focus_angle = 0.2 # this much % of the circle...
-@export var focus_intensity = 0.5 # ...has this much % of the particles.
-
 @export var focus_angle_curve: Curve
-
-@export var focus_strength = 0.3 # how much extra % of speed the focused part will have MAXIMALLY
-
 @export var focus_strength_curve: Curve
 
+var focus_strength_factor = 0
+
+var started = false
 var focus_degree = 0
 var slowed = false
 var particles = []
-var test = true
 var t = 0
+var hold_t = 0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	focus_degree = randf_range(0, 360)
-	print(focus_degree)
-	if focus_angle < 1 and focus_angle > 0:
-		focused_wave(focus_degree)
+func _process(delta):
+	if not started:
+		focus_degree = rad_to_deg((Vector2.ZERO - get_local_mouse_position()).angle()) + 180
+		focus_strength_factor = (Vector2.ZERO - get_local_mouse_position()).length() / 200
+		focus_angle_curve.set_point_value(1, 2/(1+(3*focus_strength_factor)))
+		if Input.is_action_just_released('mouseClick'):
+			wave(wave_speed, focus_angle_curve, focus_strength_curve, focus_strength_factor, total_particles, focus_degree)
+		queue_redraw()
 	else:
-		full_wave(focus_degree)
+		if not slowed: 
+			t = t + delta
+			if t * wave_speed > 20:
+				for part in particles:
+					if part.delay:
+						part.start_checking_for_slowdown()
+		if particles == []:
+			queue_free()
 
-func focused_wave(degree):
-	# the amount to 'jump forward' for every particle in the focused zone
-	var num_focused = int(num_particles * focus_intensity)
-	var num_unfocused = num_particles - num_focused
-	var particle_focus_angle = (360.0 * focus_angle) / num_focused
-	# the amount to 'jump forward' for every particle in the unfocused zone
-	var particle_regular_angle = (360.0 * (1-focus_angle)) / num_unfocused
+func _draw():
+	if not started: 
+		draw_line(Vector2.ZERO, get_local_mouse_position(), Color(0, 1, 1), 5)
+
+func wave(speed, focus_curve, strength_curve, strength_factor, num_particles, degree):
+	get_parent().wave_spawned()
+	if strength_factor > 2:
+		speed = speed / (strength_factor/2)
+	started = true
 	# to make sure the middle of the focused zone is the direction of the degree
-	var current_angle = degree - (360.0 * 0.5 * focus_angle) 
+	var current_angle = degree - (360.0 * 0.5) 
 	var num_spawned = 0
+	
+	var focus_norm = 0
+	var i = 0
+	while i < num_particles:
+		focus_norm += focus_curve.sample(i/float(num_particles))
+		i+=1
+	
 	while num_spawned < num_particles:
-		if num_spawned < num_focused:
-			make_particle(	current_angle,
-							wave_speed+
-							wave_speed*focus_strength*(focus_strength_curve.sample(num_spawned/float(num_focused))),
-							int(current_angle) == int(focus_degree))
-			#current_angle += particle_focus_angle * focus_angle_curve.sample(num_spawned/float(num_focused))
-			current_angle += particle_focus_angle
-		else:
-			make_particle(current_angle)
-			current_angle += particle_regular_angle
-		num_spawned += 1
-
-func full_wave(degree):
-	var particle_angle = 360.0/num_particles
-	var current_angle = degree
-	var num_spawned = 0
-	while num_spawned < num_particles:
-		make_particle(current_angle)
-		current_angle += particle_angle
+		make_particle(current_angle,
+					  speed +
+					  strength_factor * speed * 
+					  strength_curve.sample(num_spawned/float(num_particles)),
+					  int(current_angle) == int(degree))
+		current_angle += 360 * focus_curve.sample(num_spawned/float(num_particles)) / focus_norm
 		num_spawned += 1
 
 func make_particle(fire_angle, speed=wave_speed, tester = false):
@@ -76,17 +79,6 @@ func make_particle(fire_angle, speed=wave_speed, tester = false):
 	instance.slowdown_low_support_per_sec = slowdown_low_support_per_sec
 	add_child(instance)
 	particles.append(instance)
-	
-
-func _process(delta):
-	if not slowed: 
-		t = t + delta
-		if t * wave_speed > 20:
-			for part in particles:
-				if part.delay:
-					part.start_checking_for_slowdown()
-	if particles == []:
-		queue_free()
 
 func remove_particle(part):
 	particles.erase(part)
